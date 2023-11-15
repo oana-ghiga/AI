2c346a1e12a12eab95dfbed7632204a476ba38f3
lavinia103 <100172282+lavinia103@users.noreply.github.com>
2023-11-15 08:59:54 +0200

    update

--- h6.py
+++ h6.py
@@ -21,32 +21,36 @@ test_labels = torch.tensor(labels[int(len(labels) * 0.8):] - 1, dtype=torch.long
 
 # definirea modelului
 class NeuralNet(nn.Module):
-    def __init__(self, input_size, hidden_size, output_size):
+    def __init__(self, input_size, hidden_size1, hidden_size2, output_size):
         super(NeuralNet, self).__init__()
-        self.hidden = nn.Linear(input_size, hidden_size)
+        self.hidden1 = nn.Linear(input_size, hidden_size1)
+        self.hidden2 = nn.Linear(hidden_size1, hidden_size2)
         self.relu = nn.ReLU()
-        self.output = nn.Linear(hidden_size, output_size)
+        self.output = nn.Linear(hidden_size2, output_size)
         self.softmax = nn.Softmax(dim=1)
 
     # forward propagation
     def forward(self, x):
-        x = self.hidden(x) #only one hidden layer
-        x = self.relu(x) #reLU for input
+        x = self.hidden1(x) # first hidden layer
+        x = self.relu(x) # reLU for first hidden layer
+        x = self.hidden2(x) # second hidden layer
+        x = self.relu(x) # reLU for second hidden layer
         x = self.output(x)
-        x = self.softmax(x) #softmax for output
+        x = self.softmax(x) # softmax for output
         return x
 
 
 # initializarea parametrilor
 input_size = 7
-hidden_size = 5
+hidden_size1 = 5
+hidden_size2 = 5
 output_size = 3
 learning_rate = 0.01
 epochs = 100
 
 
 # crearea unei instante a modelului + initializarea parametrilor
-model = NeuralNet(input_size, hidden_size, output_size)
+model = NeuralNet(input_size, hidden_size1, hidden_size2, output_size)
 
 # definirea functiei de pierdere si a optimizatorului
 loss_fn = nn.CrossEntropyLoss()
@@ -109,19 +113,24 @@ with open('results.txt', 'w') as f:
     for i in range(len(test_labels)):
         f.write(f'exemplul {i+1}: clasa reala = {test_labels[i]}, clasa prezisa = {predicted[i]}\n')
 
-
 # Manual forward propagation
-hidden_weights = np.random.randn(input_size, hidden_size)
-hidden_bias = np.zeros(hidden_size)
-output_weights = np.random.randn(hidden_size, output_size)
+hidden_weights1 = np.random.randn(input_size, hidden_size1)
+hidden_bias1 = np.zeros(hidden_size1)
+hidden_weights2 = np.random.randn(hidden_size1, hidden_size2)
+hidden_bias2 = np.zeros(hidden_size2)
+output_weights = np.random.randn(hidden_size2, output_size)
 output_bias = np.zeros(output_size)
 
-# Forward pass for the hidden layer
-hidden_output = np.dot(test_features, hidden_weights) + hidden_bias
-hidden_activation = np.maximum(hidden_output, 0)  # ReLU
+# Forward pass for the first hidden layer
+hidden_output1 = np.dot(test_features, hidden_weights1) + hidden_bias1
+hidden_activation1 = np.maximum(hidden_output1, 0)  # ReLU
+
+# Forward pass for the second hidden layer
+hidden_output2 = np.dot(hidden_activation1, hidden_weights2) + hidden_bias2
+hidden_activation2 = np.maximum(hidden_output2, 0)  # ReLU
 
 # Forward pass for the output layer
-output = np.dot(hidden_activation, output_weights) + output_bias
+output = np.dot(hidden_activation2, output_weights) + output_bias
 exp_scores = np.exp(output)
 output_probs = exp_scores / np.sum(exp_scores, axis=1, keepdims=True)  # Softmax
 
@@ -134,28 +143,41 @@ grad_output = output_probs.copy()
 grad_output[np.arange(len(test_labels)), test_labels] -= 1
 grad_output /= len(test_labels)
 
-output_weights_grad = np.dot(hidden_activation.T, grad_output)
+output_weights_grad = np.dot(hidden_activation2.T, grad_output)
 output_bias_grad = np.sum(grad_output, axis=0)
 
-# Backpropagation for the hidden layer
-grad_hidden = np.dot(grad_output, output_weights.T)
-grad_hidden[hidden_output <= 0] = 0
+# Backpropagation for the second hidden layer
+grad_hidden2 = np.dot(grad_output, output_weights.T)
+grad_hidden2[hidden_output2 <= 0] = 0
 
-hidden_weights_grad = np.dot(test_features.T, grad_hidden)
-hidden_bias_grad = np.sum(grad_hidden, axis=0)
+hidden_weights2_grad = np.dot(hidden_activation1.T, grad_hidden2)
+hidden_bias2_grad = np.sum(grad_hidden2, axis=0)
+
+# Backpropagation for the first hidden layer
+grad_hidden1 = np.dot(grad_hidden2, hidden_weights2.T)
+grad_hidden1[hidden_output1 <= 0] = 0
+
+hidden_weights1_grad = np.dot(test_features.T, grad_hidden1)
+hidden_bias1_grad = np.sum(grad_hidden1, axis=0)
 
 # Update weights manually
 learning_rate = 0.01
-hidden_weights -= learning_rate * hidden_weights_grad
-hidden_bias -= learning_rate * hidden_bias_grad
+hidden_weights1 -= learning_rate * hidden_weights1_grad
+hidden_bias1 -= learning_rate * hidden_bias1_grad
+hidden_weights2 -= learning_rate * hidden_weights2_grad
+hidden_bias2 -= learning_rate * hidden_bias2_grad
 output_weights -= learning_rate * output_weights_grad
 output_bias -= learning_rate * output_bias_grad
 
 # Print updated weights for demonstration
-print("Updated Weights - Hidden Layer:")
-print(hidden_weights)
-print("Updated Bias - Hidden Layer:")
-print(hidden_bias)
+print("Updated Weights - First Hidden Layer:")
+print(hidden_weights1)
+print("Updated Bias - First Hidden Layer:")
+print(hidden_bias1)
+print("Updated Weights - Second Hidden Layer:")
+print(hidden_weights2)
+print("Updated Bias - Second Hidden Layer:")
+print(hidden_bias2)
 print("Updated Weights - Output Layer:")
 print(output_weights)
 print("Updated Bias - Output Layer:")
